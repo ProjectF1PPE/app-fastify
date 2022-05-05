c10ffee347cf85ce26ccfd3c1b5c5eb7ef8d1aab
Hugo ILARRAZ <hilarraz@stremi.fr>
2022-05-05 17:56:26 +0200

    update hugo

--- controllers/ecuries.js
+++ controllers/ecuries.js
@@ -1,7 +1,7 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
-const getEcuries = (req, reply) => {
-    const connection = mysql.createConnection({
+const getEcuries = async (req, reply) => {
+    const connection = await mysql.createConnection({
         host: 'casf1_db_1',
         user: 'root',
         password: '3M75B4fKWbsr',
@@ -10,17 +10,110 @@ const getEcuries = (req, reply) => {
 
     connection.connect();
 
-    connection.query('SELECT * from ecurie', (err, res, fields) => {
-        /*if (err) {
-            throw err;
+    /*
+
+    connection.promise().query('SELECT id, nom, photo, idPays from ecurie')
+        .then(([ecuries, fields]) => {
+            connection.promise().query('SELECT id, nom, prenom, ordre, idEcurie from pilote')
+                .then(([pilotes, pilotesFields]) => {
+                    connection.promise().query('SELECT id, nom from pays')
+                        .then(([pays, paysFields]) => {
+                            let result = [];
+
+                            console.log(ecuries);
+                            console.log(pays);
+                            console.log(pilotes);
+
+                            for (let ecurie of ecuries) {
+
+                                let nomPays = "Pays non trouvé";
+                                for (let pays of pays) {
+                                    if (pays.id === ecurie.idPays) {
+                                        nomPays = pays.nom;
+                                    }
+                                }
+
+                                let pilote1 = "";
+                                let pilote2 = "";
+                                let pilote3 = "";
+
+                                for (let pilote of pilotes) {
+                                    if (pilote.idEcurie === ecurie.id) {
+                                        if (pilote.ordre === 1) {
+                                            pilote1 = pilote.nom + " " + pilote.prenom;
+                                        } else if (pilote.ordre === 2) {
+                                            pilote2 = pilote.nom + " " + pilote.prenom;
+                                        } else if (pilote.ordre === 3) {
+                                            pilote3 = pilote.nom + " " + pilote.prenom;
+                                        }
+                                    }
+                                }
+
+                                result.push({
+                                    id: ecurie.id,
+                                    nom: ecurie.nom,
+                                    nomPays: nomPays,
+                                    pilote1: pilote1,
+                                    pilote2: pilote2,
+                                    pilote3: pilote3
+                                });
+                            }
+
+                            reply.send(result);
+                        })
+                })
+        })
+
+     */
+
+    let ecuries = await connection.query('SELECT id, nom, photo, idPays from ecurie');
+    let pilotes = await connection.query('SELECT id, nom, prenom, ordre, idEcurie from pilote');
+    let pays = await connection.query('SELECT id, nom from pays');
+
+    let result = [];
+
+    console.log(ecuries);
+    console.log(pays);
+    console.log(pilotes);
+
+    for (const ecurie of ecuries) {
+        console.log("ecurie: " + ecurie.id + " - " + ecurie.nom);
+
+        let nomPays = "Pays non trouvé";
+        for (const unPays of pays) {
+            console.log(unPays);
+
+            if (unPays.id == ecurie.idPays) {
+                nomPays = unPays.nom;
+            }
         }
 
-         */
-        reply.send(res);
-        console.log('err: ' + err);
-        console.log('results: ' + res);
-        console.log('fetched: ' + fields);
-    })
+        let pilotes = [];
+
+        for (const pilote of pilotes) {
+            if (pilote.idEcurie == ecurie.id) {
+                pilotes.push(pilote.nom + " " + pilote.prenom);
+            }
+        }
+
+        result.push({
+            "id": ecurie.id,
+            "nom": ecurie.nom,
+            "nomPays": nomPays,
+            "pilotes": pilotes
+        });
+    }
+
+    console.log(result);
+
+    reply.send(result);
+
+    // id ecurie
+    // nom ecurie
+    // nom du pays
+    // pilote1
+    // pilote2
+    // pilote3
 
     connection.end();
 }
