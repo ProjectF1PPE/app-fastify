b13350619085e55d91d40dc38c7af8e090d1aae8
Dimitri RICQUIER <dricquier@stremi.fr>
2022-05-06 13:38:07 +0200

    test

---
+++ public/pages/resultat/resultat.js
@@ -0,0 +1,26 @@
+﻿"use strict";
+
+window.onload = init
+
+function init() {
+    $.ajax({
+        url: 'ajax/getlesequipes.php',
+        type: 'POST',
+        dataType: "json",
+        error: response => console.error(response.responseText),
+        success: afficher
+    });
+}
+
+/**
+ * demande d'ajout dans la base de données
+ */
+function afficher(data) {
+    for (const element of data) {
+        let tr = lesLignes.insertRow();
+        tr.insertCell().innerText = element.id;
+        tr.insertCell().innerText = element.nom;
+        tr.insertCell().innerText = element.groupe;
+    }
+}
+
