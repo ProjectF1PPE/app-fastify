7bdc9d0c6911b283b3ba36801e734ca70ee3e4dc
Dimitri RICQUIER <dricquier@stremi.fr>
2022-05-06 16:23:41 +0200

    dim

--- public/pages/resultat/ecurie/resultat.js
+++ public/pages/resultat/ecurie/resultat.js
@@ -11,14 +11,14 @@ async function init() {
     }
 }
 
-function remplirLesGrandsPrix(data) {
+async function remplirLesGrandsPrix(data) {
     for (const gp of data) {
         // ajouter le gp à la liste qui affiche les gp
 
         // mettre un event de on click
         // faire ca
         try {
-            const data = (await axios.get("/api/resultats/pilotes/", { params: {gp: gp.id }})).data;
+            const data = (await axios.get("/api/resultats/ecurie/", { params: {gp: gp.id }})).data;
             afficher(data);
         } catch(e) {
             throw e;
@@ -31,17 +31,20 @@ function remplirLesGrandsPrix(data) {
  */
 function afficher(data) {
     for (const element of data) {
-        let tr = lesLignes.insertRow();
-
+        let tr = document.getElementById('lesLignes').insertRow();
 
         // place
+        tr.insertCell().innerText = element.place;
         // img
+        tr.insertCell().innerText = element.img;
         // element.nom = nom + prenom
-        // id pays
-        // point
-        tr.insertCell().innerText = element.id;
         tr.insertCell().innerText = element.nom;
-        tr.insertCell().innerText = element.groupe;
+        // id pays
+        tr.insertCell().innerText = element.idPays;
+        // point
+        tr.insertCell().innerText = element.point;
+
     }
 }
+
 
--- public/pages/resultat/pilote/resultat.js
+++ public/pages/resultat/pilote/resultat.js
@@ -2,14 +2,28 @@
 
 window.onload = init
 
-function init() {
-    $.ajax({
-        url: 'ajax/getlesequipes.php',
-        type: 'POST',
-        dataType: "json",
-        error: response => console.error(response.responseText),
-        success: afficher
-    });
+async function init() {
+    try {
+        const data = (await axios.get("/api/resultats/gp")).data;
+        remplirLesGrandsPrix(data);
+    } catch(e) {
+        throw e;
+    }
+}
+
+async function remplirLesGrandsPrix(data) {
+    for (const gp of data) {
+        // ajouter le gp à la liste qui affiche les gp
+
+        // mettre un event de on click
+        // faire ca
+        try {
+            const data = (await axios.get("/api/resultats/pilotes/", { params: {gp: gp.id }})).data;
+            afficher(data);
+        } catch(e) {
+            throw e;
+        } // finir l'event
+    }
 }
 
 /**
@@ -17,10 +31,25 @@ function init() {
  */
 function afficher(data) {
     for (const element of data) {
-        let tr = lesLignes.insertRow();
-        tr.insertCell().innerText = element.id;
+        let tr = document.getElementById('lesLignes').insertRow();
+
+        // place
+        tr.insertCell().innerText = element.place;
+        // img
+        let img = new Image();
+        img.src = "pages/pilote/img/" + element.id + ".png";
+        img.onerror = () => {
+            img.src = "pages/img/1.png"
+        }
+
+        tr.insertCell(1).appendChild(img)
+        // element.nom = nom + prenom
         tr.insertCell().innerText = element.nom;
-        tr.insertCell().innerText = element.groupe;
+        // id pays
+        tr.insertCell().innerText = element.idPays;
+        // point
+        tr.insertCell().innerText = element.point;
+
     }
 }
 
