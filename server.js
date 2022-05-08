af5e9533e9e7b64536a2c7a85cb93244ab1b2727
Violet <62477185+Violetow9@users.noreply.github.com>
2022-05-08 15:21:30 +0200

    update hugo

--- server.js
+++ server.js
@@ -4,16 +4,6 @@ const fastify = require('fastify')({
 const path = require("path");
 const fastifyStatic = require("fastify-static");
 
-const mysql = require('mysql2');
-
-/*
-fastify.register(require('fastify-mysql'), {
-    promise: true,
-    connectionString: 'mysql://root:3M75B4fKWbsr@casf1_db_1:3306/f1'
-});
-
- */
-
 fastify.register(require('fastify-swagger'), {
     exposeRoute: true,
     routePrefix: '/docs',
@@ -36,68 +26,6 @@ fastify.register(require('./routes/resultats/gp'));
 fastify.register(require('./routes/resultats/pilote'));
 fastify.register(require('./routes/resultats/admin/resultat'));
 
-/*
-fastify.mysql.query('SELECT * from ecurie', (err, result) => {
-    client.release();
-    reply.send(result);
-    //reply.send(err || result);
-});
-
-/*
-fastify.register(fastifyStatic, {
-    root: path.join(__dirname, 'public', 'pages'),
-    prefix: '/pages/',
-    list: true,
-    decorateReply: false
-})
-
- */
-
-/*
-fastify.register(fastifyStatic, {
-    root: path.join(__dirname, 'public', 'pages'),
-    prefix: '/pages/',
-    decorateReply: false
-})
-
-/*
-fastify.register(fastifyStatic, {
-    root: path.join(__dirname, 'public', 'pages', 'gp', 'circuit'),
-    prefix: '/pages/gp/circuit/',
-    list: true,
-    decorateReply: false
-})
-
-/*
-fastify.register(fastifyStatic, {
-    root: path.join(__dirname, 'public', 'pages'),
-    decorateReply: false
-});
-
-/*
-fastify.register(fastifyStatic, {
-    root: path.join(__dirname, 'public', 'pages', 'ecurie'),
-    decorateReply: false
-});
-
-/*
-
-fastify.register(fastifyStatic, {
-    root: path.join(__dirname, 'public', 'pages', 'pilote'),
-    decorateReply: false
-});
-
-fastify.register(fastifyStatic, {
-    root: path.join(__dirname, 'public', 'modules', 'footer'),
-    decorateReply: false
-});
-
-fastify.register(fastifyStatic, {
-    root: path.join(__dirname, 'public', 'modules', 'navbar'),
-    decorateReply: false
-});
- */
-
 fastify.get('/insomnia', async (request, reply) => {
     reply.sendFile('insomnia.json');
 });
@@ -105,13 +33,6 @@ fastify.get('/insomnia', async (request, reply) => {
 fastify.get('/', async (request, reply) => {
     reply.sendFile('index.html');
 });
-
-/*
-fastify.get('/ecuries', async (request, reply) => {
-    reply.sendFile('ecuries.html', path.join(__dirname, 'public', 'pages', 'ecurie'));
-});
-
- */
 
 fastify.get('/ecuries', async (request, reply) => {
     reply.sendFile('pages/ecurie/ecuries.html');
@@ -125,18 +46,13 @@ fastify.get('/gp', async (request, reply) => {
     reply.sendFile('pages/gp/gp.html');
 });
 
+fastify.get('/classement/pilotes', async (request, reply) => {
+    reply.sendFile('pages/classement/classementPilote.html');
+});
+
 fastify.get('/admin/ecurie', async (request, reply) => {
     reply.sendFile('pages/ecurie/admin/ecurieAdmin.html');
 });
-
-/*
-fastify.get('/ecurie', async (request, reply) => {
-    const connection = await fastify.mysql.getConnection()
-    const [rows] = await connection.query('Select id, nom, prenom, ordre from pilote order by id')
-    connection.release()
-    return rows[0]
-});
- */
 
 const start = async () => {
     try {
